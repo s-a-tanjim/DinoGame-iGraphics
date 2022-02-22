6ccca2d6910fd380c8b96d3e460335e27d84823e
Shoeb Ahmed Tanjim <39959228+s-a-tanjim@users.noreply.github.com>
2022-02-22 17:36:10 +0600

    Add files via upload

---
+++ final/all_variables.h
@@ -0,0 +1,22 @@
+int windowWidth = 630, windowHeight = 400;
+
+unsigned long int HIGHEST_SCORE = 0;
+
+bool showInitDisplay = true;
+
+int BaseHeight = 150;
+int groundWidth = 90;
+float initialGroundPoint = -50;
+
+char dino_img[4][50] = {
+	"bitmaps\\dino\\dino_default.bmp",
+	"bitmaps\\dino\\dino_ducking_leftstep.bmp",
+	"bitmaps\\dino\\dino_default.bmp",
+	"bitmaps\\dino\\dino_default.bmp"
+};
+
+char ground_img[3][50] = {
+	"bitmaps\\ground\\ground_0.bmp",
+	"bitmaps\\ground\\cactus_0.bmp",
+	"bitmaps\\ground\\cactus_3.bmp",
+};
